f7bc1cba8484f5bc7adf829c8d6b0361c8bdba84
Willy Prado <williamnyprado@gmail.com>
2021-02-17 09:02:44 -0500

    0. 98 Batería st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
-* reset_to_98 - change the value of *n to 98
-* @n: is a pointer
-*    
-*/
+ * reset_to_98 - change the value of *n to 98
+ * @n: is a pointer
+ *    
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
