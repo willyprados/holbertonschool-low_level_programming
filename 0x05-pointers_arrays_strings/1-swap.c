0a5429b3a8065fb6f7e632f1ca254304c826285e
Willy Prado <williamnyprado@gmail.com>
2021-02-18 12:41:50 -0500

    settings in the exercise 0 & 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,25 +1,19 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * int main - swaps the values of two integers
+ * swap_int - swaps the values of two integers
  * @a: is a pointer
  * @b: is a pointer
  * @c: is a variable
  * Return: 0;
  */
-int main()
+void swap_int(int *a, int *b)
 {
-	int a = 98;
-	int b = 42;
-	int c;         
-
-	printf("a=%d, b=%d\n", a, b);
-
-	c = *&a;
-	a = *&b;
-	b = c;
-
-	printf("a=%d, b=%d\n", a, b);
-	return (0);
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+	return;
 }
